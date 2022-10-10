b2ba2fdc05bb42390b9af22349664d8d0468e9ce
Achris54 <57518328+Achris54@users.noreply.github.com>
2022-10-10 16:56:48 -0400

    Initial website design
    this contains files for creating the initial webpage and includes the Navbar component

---
+++ src/components/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../../App";
+//import Cards from "../Cards";
+//import HeroSection from "../HeroSection";
+//import Footer from "../Footer";
+
+function Home() {
+  return <></>;
+}
+
+export default Home;
